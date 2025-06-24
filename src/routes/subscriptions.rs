412827c1aa70fbd241eeaf3967d0177af4d01ebc
Abeeujah <abeeujah@gmail.com>
2025-06-24 17:58:14 +0100

    Naive Approach

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -129,7 +129,7 @@ pub async fn send_confirmation_email(
     );
 
     email_client
-        .send_email(new_subscriber.email, "Welcome!", &html_body, &text_body)
+        .send_email(&new_subscriber.email, "Welcome!", &html_body, &text_body)
         .await
 }
 
@@ -210,7 +210,7 @@ impl ResponseError for SubscribeError {
     }
 }
 
-fn error_chain_fmt(
+pub fn error_chain_fmt(
     e: &impl std::error::Error,
     f: &mut std::fmt::Formatter<'_>,
 ) -> std::fmt::Result {
